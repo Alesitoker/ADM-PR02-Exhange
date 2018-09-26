0d9055f6ac3118c594b6e10860a6b30918b93b56
Alejandro <alesitonightray@gmail.com>
2018-09-26 10:36:22 +0200

    Initial commit

---
+++ app/src/main/java/com/iessaladillo/alejandro/adm_pr02_exhange/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iessaladillo.alejandro.adm_pr02_exhange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
